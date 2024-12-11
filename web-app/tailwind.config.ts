808301fe637b10e8ec58a754963326a36c7c1e3e
DStojanac <d.stojanac2002@gmail.com>
2024-12-11 17:39:50 +0100

    Add font and brand colors

--- web-app/tailwind.config.ts
+++ web-app/tailwind.config.ts
@@ -9,8 +9,17 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "main-background": "var(--main-background)",
+        "second-background": "var(--second-background)",
+        "main-btn": "var(--main-btn)",
+        "second-btn": "var(--second-btn)",
+        "main-paragraph-text": "var(--main-paragraph-text)",
+        "second-paragraph-text": "var(--second-paragraph-text)",
+        "white-text": "var(--white-text)",
+        "black-text": "var(--black-text)",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
       },
     },
   },
