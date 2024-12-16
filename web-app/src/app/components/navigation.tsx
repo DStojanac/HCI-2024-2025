b62f58bf52d654992e4ff97caf596abfdff3a982
DStojanac <d.stojanac2002@gmail.com>
2024-12-16 21:36:41 +0100

    Refactor navbar for better responsivness and add hamburger icon

--- web-app/src/app/components/navigation.tsx
+++ web-app/src/app/components/navigation.tsx
@@ -32,12 +32,47 @@ const pages: Page[] = [
   },
 ];
 
-function processPage(page: Page, index: number, pathName: string) {
+function Hamburger() {
   return (
-    <li key={index}>
+    <button
+      className="flex md:hidden flex-col justify-center items-end w-11 h-11 p-2 space-y-1.5 rounded-sm "
+      aria-label="Open menu"
+    >
+      <span className="w-7 h-1 bg-main-btn rounded-full" />
+      <span className="w-4 h-1 bg-main-btn rounded-full" />
+      <span className="w-6 h-1 bg-main-btn rounded-full" />
+    </button>
+  );
+}
+
+function processPage(page: Page, index: number, pathName: string) {
+  const isLogin = page.path === "/login";
+  const isSignUp = page.path === "/signup";
+  const isActive = page.path === pathName;
+
+  return (
+    <li className="hover:text-navigation-text" key={index}>
       <Link
         href={page.path}
-        className={pathName === page.path ? "font-bold" : ""}
+        className={`
+            ${
+              isActive && !isLogin && !isSignUp
+                ? "font-bold text-navigation-text"
+                : isActive
+                ? "text-navigation-text"
+                : ""
+            }
+          ${
+            isLogin
+              ? "  text-second-paragraph-text border border-second-paragraph-text px-5 py-2.5 rounded-md hover:bg-gray-100"
+              : ""
+          }
+          ${
+            isSignUp
+              ? "bg-main-btn text-white px-5 py-2.5  border border-main-background  rounded-md hover:bg-second-btn"
+              : ""
+          }
+        `}
       >
         {page.title}
       </Link>
@@ -49,8 +84,9 @@ export function Navigation() {
   const pathName = usePathname();
   return (
     <>
-      <ul className="flex space-x-4 mb-4">
+      <ul className=" flex md:space-x-4 lg:space-x-9 text-sm md:text-base  ">
         {pages.map((page, index) => processPage(page, index, pathName))}
+        <Hamburger />
       </ul>
     </>
   );
--- web-app/src/app/page.tsx
+++ web-app/src/app/page.tsx
@@ -3,14 +3,16 @@ import { Navigation } from "./components/navigation";
 export default function Home() {
   return (
     <>
-      <main className="flex flex-col min-h-screen">
-        <div className="flex justify-between pt-8 px-8">
-          <div>COOKSY</div>
+      <main className="flex flex-col min-h-screen ">
+        <div className=" md:h-[96px] flex justify-between items-center lg:px-20 md:px-10 sm:px-5">
+          <div className="text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-[#3C3128] via-[#A4330D] to-[#A4330D]">
+            COOKSY
+          </div>
           <Navigation />
         </div>
         <div className="flex flex-grow  items-center justify-center">
-          <h1 className=" text-6xl font-extrabold tracking-tight text-center">
-            Home page
+          <h1 className="text-6xl font-extrabold tracking-tight text-center">
+            Home Page
           </h1>
         </div>
       </main>
