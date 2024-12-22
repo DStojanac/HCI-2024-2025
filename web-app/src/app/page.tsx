aa8131761aef65ca0c820d5f435d1f7d6df192eb
DStojanac <d.stojanac2002@gmail.com>
2024-12-22 11:39:38 +0100

    Updated home page and included two new benefits sections

--- web-app/src/app/page.tsx
+++ web-app/src/app/page.tsx
@@ -1,10 +1,18 @@
+import { Save } from "lucide-react";
 import Hero from "./components/layout/Hero";
+import SaveAndOrganize from "./components/layout/SaveAndOrganize";
+import Benefits from "./components/layout/Benefits";
 
 export default function Home() {
   return (
-    <>
+    <main>
       <Hero />
-      <main className="container mx-auto max-w-full px-4 lg:px-20 md:px-10 sm:px-5"></main>
-    </>
+      <div className="container mx-auto max-w-full px-4 lg:px-20 md:px-10 sm:px-5">
+        <SaveAndOrganize />
+      </div>
+      <div className="bg-second-background">
+        <Benefits />
+      </div>
+    </main>
   );
 }
