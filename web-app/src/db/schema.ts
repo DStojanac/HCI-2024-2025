2f87309943819804520268d1dcb95cbe0d1c0d1e
DStojanac <d.stojanac2002@gmail.com>
2025-01-05 20:01:07 +0100

    Add users and accounts schema

--- web-app/src/db/schema.ts
+++ web-app/src/db/schema.ts
@@ -1,4 +1,12 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  serial,
+  text,
+  varchar,
+  integer,
+  primaryKey,
+} from "drizzle-orm/pg-core";
+import type { AdapterAccountType } from "next-auth/adapters";
 
 export const recipe = pgTable("recipe", {
   id: serial("id").primaryKey(),
@@ -7,3 +15,39 @@ export const recipe = pgTable("recipe", {
   ingredients: text("ingredients").notNull(),
   difficulty: text("difficulty").notNull(),
 });
+
+export const users = pgTable("users", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  name: varchar("name", { length: 255 }).notNull(),
+  email: text("email").notNull().unique(),
+  password: text("password").notNull(),
+  image: text("image"),
+});
+
+export const accounts = pgTable(
+  "account",
+  {
+    userId: text("userId")
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    type: text("type").$type<AdapterAccountType>().notNull(),
+    provider: text("provider").notNull(),
+    providerAccountId: text("providerAccountId").notNull(),
+    refresh_token: text("refresh_token"),
+    access_token: text("access_token"),
+    expires_at: integer("expires_at"),
+    token_type: text("token_type"),
+    scope: text("scope"),
+    id_token: text("id_token"),
+    session_state: text("session_state"),
+  },
+  (account) => [
+    {
+      compoundKey: primaryKey({
+        columns: [account.provider, account.providerAccountId],
+      }),
+    },
+  ]
+);
