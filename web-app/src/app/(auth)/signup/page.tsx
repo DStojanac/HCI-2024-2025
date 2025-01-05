bf736311a71b9e9fea5842d7069adb02104ad8c4
DStojanac <d.stojanac2002@gmail.com>
2025-01-05 19:59:16 +0100

    Implemented fully responsive and authentication working signup page

--- web-app/src/app/(auth)/signup/page.tsx
+++ web-app/src/app/(auth)/signup/page.tsx
@@ -1,8 +1,64 @@
+"use client";
+
+import * as z from "zod";
+
+import { useState, useTransition } from "react";
 import Link from "next/link";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SignUpSchema } from "@/lib/validations";
+import { FormError } from "@/components/form-error";
+import { FormSuccess } from "@/components/form-success";
+import { signup } from "@/actions/signup";
+import { useRouter } from "next/navigation";
 
 export default function SignUpPage() {
+  const router = useRouter();
+
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof SignUpSchema>>({
+    resolver: zodResolver(SignUpSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof SignUpSchema>) => {
+    setError("");
+    setSuccess("");
+
+    startTransition(() => {
+      signup(values).then((data) => {
+        if (data.error) {
+          return setError(data.error);
+        }
+        if (data.success) {
+          setSuccess(data.success);
+
+          setTimeout(() => {
+            router.push("/login");
+          }, 1500);
+        }
+      });
+    });
+  };
+
   return (
     <div className="min-h-screen flex flex-col">
       {/* Header */}
@@ -38,47 +94,77 @@ export default function SignUpPage() {
             </p>
           </div>
 
-          <div className="space-y-4">
-            <div className="space-y-2">
-              <label className="block text-sm" htmlFor="name">
-                Name
-              </label>
-              <Input
-                id="name"
-                placeholder="Enter your name"
-                type="text"
-                className="w-full border-input border-main-paragraph-text rounded-none focus:border-main-btn focus:ring-main-btn "
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="block text-sm">Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isPending}
+                        placeholder="Enter your name"
+                        {...field}
+                        className="w-full border-input border-main-paragraph-text rounded-none focus:border-main-btn focus:ring-main-btn"
+                      />
+                    </FormControl>
+                    <FormMessage className="text-navigation-text" />
+                  </FormItem>
+                )}
               />
-            </div>
 
-            <div className="space-y-2">
-              <label className="block text-sm" htmlFor="email">
-                Email
-              </label>
-              <Input
-                id="email"
-                placeholder="Enter your email"
-                type="email"
-                className="w-full border-input border-main-paragraph-text rounded-none focus:border-main-btn focus:ring-main-btn "
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="block text-sm">Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isPending}
+                        placeholder="Enter your email"
+                        type="email"
+                        {...field}
+                        className="w-full border-input border-main-paragraph-text rounded-none focus:border-main-btn focus:ring-main-btn"
+                      />
+                    </FormControl>
+                    <FormMessage className="text-navigation-text" />
+                  </FormItem>
+                )}
               />
-            </div>
 
-            <div className="space-y-2">
-              <label className="block text-sm" htmlFor="password">
-                Password
-              </label>
-              <Input
-                id="password"
-                placeholder="Enter your password"
-                type="password"
-                className="w-full border-input border-main-paragraph-text rounded-none focus:border-main-btn focus:ring-main-btn "
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="block text-sm">Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isPending}
+                        placeholder="********"
+                        type="password"
+                        {...field}
+                        className="w-full border-input border-main-paragraph-text rounded-none focus:border-main-btn focus:ring-main-btn"
+                      />
+                    </FormControl>
+                    <FormMessage className="text-navigation-text" />
+                  </FormItem>
+                )}
               />
-            </div>
-
-            <Button className="w-full bg-main-btn hover:bg-second-btn text-white py-6">
-              Sign Up
-            </Button>
-          </div>
+              <FormError message={error} />
+              <FormSuccess message={success} />
+              <Button
+                disabled={isPending}
+                type="submit"
+                className="w-full bg-main-btn hover:bg-second-btn text-white py-6"
+              >
+                Sign Up
+              </Button>
+            </form>
+          </Form>
 
           <div className="text-center text-sm text-muted-foreground mt-8">
             © 2024 Cooksy
