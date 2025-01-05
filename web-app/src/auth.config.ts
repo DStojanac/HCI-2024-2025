90c16df8ae43dc23c695d9038bbee29f29bd9c17
DStojanac <d.stojanac2002@gmail.com>
2025-01-05 20:04:22 +0100

    Implemented authorize with credentials and session and jwt callback functions

---
+++ web-app/src/auth.config.ts
@@ -0,0 +1,12 @@
+import type { NextAuthConfig } from "next-auth";
+// import Credentials from "next-auth/providers/credentials";
+
+// import bcrypt from "bcryptjs";
+// import { LoginSchema } from "./lib/validations";
+// import { db } from "./db";
+// import { users } from "./db/schema";
+// import { eq } from "drizzle-orm";
+
+export default {
+  providers: [],
+} satisfies NextAuthConfig;
--- web-app/src/auth.ts
+++ web-app/src/auth.ts
@@ -1,5 +1,60 @@
 import NextAuth from "next-auth";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "@/db";
+import authConfig from "@/auth.config";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import { LoginSchema } from "@/lib/validations";
+import { users } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { User } from "next-auth";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      return session;
+    },
+
+    async jwt({ token }) {
+      return token;
+    },
+  },
+  adapter: DrizzleAdapter(db),
+  session: { strategy: "jwt" },
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials): Promise<User | null> {
+        const validatedFields = LoginSchema.safeParse(credentials);
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+          const user = await db
+            .select()
+            .from(users)
+            .where(eq(users.email, email))
+            .execute();
+          if (!user.length || !user[0].password) {
+            return null;
+          }
+          const passwordsMatch = await bcrypt.compare(
+            password,
+            user[0].password
+          );
+          if (!passwordsMatch) {
+            return null;
+          }
+          return {
+            id: user[0].id,
+            email: user[0].email,
+            name: user[0].name,
+          };
+        }
+        return null;
+      },
+    }),
+  ],
 });
