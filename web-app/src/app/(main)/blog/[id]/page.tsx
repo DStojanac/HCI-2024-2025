d0d097a9f76ac657537bbdbc1630a22fcc330aaa
kovacict <115424105+kovacict@users.noreply.github.com>
2025-01-21 16:08:58 +0100

    Fix typescript error

--- web-app/src/app/(main)/blog/[id]/page.tsx
+++ web-app/src/app/(main)/blog/[id]/page.tsx
@@ -1,19 +1,14 @@
 import { BlogRecipePost } from "../page";
 
-type BlogPageProps = {
-  params: {
-    id: string;
-  };
-};
-
 async function getBlogPost(id: string): Promise<BlogRecipePost> {
   const response = await fetch(`${process.env.BASE_API_URL}/recipes/${id}`);
   const data = response.json();
   return data;
 }
 
-export default async function BlogPage({ params }: BlogPageProps) {
-  const blogPost = await getBlogPost(params.id);
+export default async function BlogPage({params}: {params: Promise<{ id: string }>}) {
+  const id=(await params).id;
+  const blogPost = await getBlogPost(id);
   // console.log(blogPost);
   return (
     <>
