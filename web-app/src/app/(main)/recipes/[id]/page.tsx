6dd007cef70341b82d8539286f021eb8f124db93
kovacict <115424105+kovacict@users.noreply.github.com>
2025-01-21 16:45:51 +0100

    Update dynamic recipe page to work with cms

--- web-app/src/app/(main)/recipes/[id]/page.tsx
+++ web-app/src/app/(main)/recipes/[id]/page.tsx
@@ -4,54 +4,31 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Clock, Users, ChefHat, Heart, Star } from "lucide-react";
+import { RECIPE_ID_QUERY } from "@/sanity/lib/queries";
+import { client } from "@/sanity/lib/client";
+import { urlFor } from "@/sanity/lib/image";
 
-type recipePost = {
-  id: number;
-  name: string;
-  ingredients: string[];
-  instructions: string[];
-  prepTimeMinutes: number;
-  cookTimeMinutes: number;
-  servings: number;
-  difficulty: "Easy" | "Medium" | "Hard";
-  cuisine: string;
-  caloriesPerServing: number;
-  tags: string[];
-  userId: number;
-  image: string;
-  rating: number;
-  reviewCount: number;
-  mealType: string[];
-};
 
-async function getRecipePost(id: string): Promise<recipePost> {
-  const response = await fetch(`${process.env.BASE_API_URL}/recipes/${id}`);
-  const data = response.json();
-  return data;
-}
 
-export default async function RecipePage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const recipe = await getRecipePost(params.id);
+export default async function RecipePage({params}: {params: Promise<{ id: string }>}) {
+  const id=(await params).id;
+  const recipe=await client.fetch(RECIPE_ID_QUERY, {id});
 
   return (
-    <div className="bg-main-background container mx-auto px-20 py-10">
+    <div className="bg-main-background container mx-auto px-12 py-10 xl:px-20">
       <div className="grid md:grid-cols-2 gap-8">
         {/* Left Column: Image and Quick Info */}
         <div className="space-y-6">
           <div className="relative aspect-video rounded-lg overflow-hidden">
             <Image
-              src={recipe.image}
-              alt={recipe.name}
+              src={urlFor(recipe.mainImage).url()}
+              alt={recipe.title}
               fill
               className="object-cover"
             />
           </div>
           <div className="flex justify-between items-center text-main-paragraph-text">
-            <h1 className="text-3xl font-bold">{recipe.name}</h1>
+            <h1 className="text-3xl font-bold">{recipe.title}</h1>
             <div className="flex space-x-4 ">
               <Button
                 variant="outline"
@@ -62,13 +39,14 @@ export default async function RecipePage({
               </Button>
             </div>
           </div>
+          <div className="text-second-paragraph-text">{recipe.description}</div>
           <div className="flex flex-wrap gap-4 text-main-paragraph-text">
             <Badge
               variant="secondary"
               className="flex items-center gap-1 bg-second-background rounded-full"
             >
               <Clock className="h-4 w-4" />
-              {recipe.prepTimeMinutes + recipe.cookTimeMinutes} mins
+              {recipe.cookingTime} mins
             </Badge>
             <Badge
               variant="secondary"
@@ -85,24 +63,24 @@ export default async function RecipePage({
               {recipe.difficulty}
             </Badge>
           </div>
-          <div className="flex items-center space-x-2">
+          {/* {<div className="flex items-center space-x-2">
             {[...Array(5)].map((_, i) => (
               <Star
                 key={i}
                 className={`h-5 w-5 ${
-                  i < Math.floor(recipe.rating)
+                  i < Math.floor(5)
                     ? "text-second-background fill-current"
                     : "text-main-paragraph-text "
                 }`}
               />
             ))}
             <span className="font-medium text-second-paragraph-text">
-              {recipe.rating}
+              {5}
             </span>
             <span className="text-muted-foreground text-second-paragraph-text">
-              ({recipe.reviewCount} reviews)
+              ({5} reviews)
             </span>
-          </div>
+          </div> } */}
         </div>
 
         {/* Right Column: Ingredients and Instructions */}
@@ -125,24 +103,26 @@ export default async function RecipePage({
             <TabsContent value="ingredients">
               <Card className="border-second-paragraph-text">
                 <CardContent className="pt-6 ">
-                  <ul className="list-disc list-inside space-y-2 text-main-paragraph-text">
-                    {recipe.ingredients.map((ingredient, index) => (
-                      <li key={index}>{ingredient}</li>
-                    ))}
-                  </ul>
+                   <ul className="list-disc list-inside space-y-2 text-main-paragraph-text">
+                   {recipe.ingredients.map((ingredient, index) => (
+                  <li key={ingredient._key}>
+                    {ingredient.amount} {ingredient.item}
+                  </li>
+                ))}
+                  </ul> 
                 </CardContent>
               </Card>
             </TabsContent>
             <TabsContent value="instructions">
               <Card className="border-second-paragraph-text">
                 <CardContent className="pt-6 border-second-paragraph-text">
-                  <ol className="list-decimal list-inside space-y-4 text-main-paragraph-text">
+                   <ol className="list-decimal list-inside space-y-4 text-main-paragraph-text">
                     {recipe.instructions.map((step, index) => (
                       <li key={index} className="pl-2">
                         {step}
                       </li>
                     ))}
-                  </ol>
+                  </ol> 
                 </CardContent>
               </Card>
             </TabsContent>
@@ -156,7 +136,7 @@ export default async function RecipePage({
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4">
           <Card>
             <CardContent className="p-4 text-center">
-              <p className="text-lg font-medium">100</p>
+              <p className="text-lg font-medium">{recipe.nutrition.calories}</p>
               <p className="text-sm text-muted-foreground capitalize">
                 Calories
               </p>
@@ -164,19 +144,19 @@ export default async function RecipePage({
           </Card>
           <Card>
             <CardContent className="p-4 text-center">
-              <p className="text-lg font-medium">100</p>
+              <p className="text-lg font-medium">{recipe.nutrition.fat}</p>
               <p className="text-sm text-muted-foreground capitalize">Fat</p>
             </CardContent>
           </Card>
           <Card>
             <CardContent className="p-4 text-center">
-              <p className="text-lg font-medium">100</p>
+              <p className="text-lg font-medium">{recipe.nutrition.carbs}</p>
               <p className="text-sm text-muted-foreground capitalize">Carbs</p>
             </CardContent>
           </Card>
           <Card>
             <CardContent className="p-4 text-center">
-              <p className="text-lg font-medium">100</p>
+              <p className="text-lg font-medium">{recipe.nutrition.protein}</p>
               <p className="text-sm text-muted-foreground capitalize">
                 Protein
               </p>
@@ -184,7 +164,7 @@ export default async function RecipePage({
           </Card>
           <Card>
             <CardContent className="p-4 text-center">
-              <p className="text-lg font-medium">100</p>
+              <p className="text-lg font-medium">{recipe.nutrition.fiber}</p>
               <p className="text-sm text-muted-foreground capitalize">Fiber</p>
             </CardContent>
           </Card>
@@ -196,14 +176,14 @@ export default async function RecipePage({
         <div className="relative w-16 h-16 rounded-full overflow-hidden">
           <Image
             src="/placeholder.svg"
-            alt={recipe.userId.toString()}
+            alt={recipe.author.name}
             fill
             className="object-cover"
           />
         </div>
         <div>
           <p>Recipe by</p>
-          <p className="text-lg font-semibold">{recipe.userId}</p>
+          <p className="text-lg font-semibold">{recipe.author.name}</p>
         </div>
       </div>
     </div>
