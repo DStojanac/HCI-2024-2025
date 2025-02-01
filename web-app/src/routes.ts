d1251d2ebae973753f7b1759d8ef70447ea4a714
DStojanac <d.stojanac2002@gmail.com>
2025-02-01 18:23:14 +0100

    Changed default login redirect to homepage

--- web-app/src/routes.ts
+++ web-app/src/routes.ts
@@ -31,4 +31,4 @@ export const apiAuthPrefix = "/api/auth";
 /**
  * Default redirect path after login
  */
-export const DEFAULT_LOGIN_REDIRECT = "/dashboard";
+export const DEFAULT_LOGIN_REDIRECT = "/";
